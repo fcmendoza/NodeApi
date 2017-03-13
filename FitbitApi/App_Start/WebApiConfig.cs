117f06827ebf5ad68d457d646525320f84b22edf
Fernando Mendoza <fcmendoza@gmail.com>
2017-03-13 15:27:49 -0700

    First commit

---
+++ FitbitApi/App_Start/WebApiConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Http;
+
+namespace FitbitApi.App_Start
+{
+    public class WebApiConfig
+    {
+        public static void Configure(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            // This will prevent any xml formatting for responses. If we end up needing that in the future there are other workarounds
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+        }
+    }
+}
---
+++ FitbitApi/Controllers/FitbitController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace FitbitApi.Controllers
+{
+    public class FitbitController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Summaries([FromUri] SummaryRequest request)
+        {
+            var summaries = new List<FoodSummary> {
+                new FoodSummary { Date = DateTime.Now.Date.AddDays(-5), CaloriesTotal = 1500, ProteinTotal = 80, CarbsTotal = 180 },
+                new FoodSummary { Date = DateTime.Now.Date.AddDays(-4), CaloriesTotal = 1700, ProteinTotal = 100, CarbsTotal = 200 },
+            };
+            return Ok(summaries);
+        }
+
+        [Route("customers/{customerId}/orders")]
+        [HttpGet]
+        public IEnumerable<FoodSummary> FindOrdersByCustomer(int customerId)
+        {
+            var summaries = new List<FoodSummary> {
+                new FoodSummary { Date = DateTime.Now.Date.AddDays(-5), CaloriesTotal = 1500, ProteinTotal = 80, CarbsTotal = 180 },
+                new FoodSummary { Date = DateTime.Now.Date.AddDays(-4), CaloriesTotal = 1700, ProteinTotal = 100, CarbsTotal = 200 },
+            };
+            return summaries;
+        }
+
+        [HttpGet, Route("fitbit/summaries")]
+        public IHttpActionResult GetSummaries([FromUri] SummaryRequest request)
+        {
+            var summaries = new List<FoodSummary> {
+                new FoodSummary { Date = DateTime.Now.Date.AddDays(-5), CaloriesTotal = 1500, ProteinTotal = 80, CarbsTotal = 180 },
+                new FoodSummary { Date = DateTime.Now.Date.AddDays(-4), CaloriesTotal = 1700, ProteinTotal = 100, CarbsTotal = 200 },
+            };
+            return Ok(summaries);
+        }
+    }
+
+    public class SummaryRequest
+    {
+        public DateTime? From { get; set; }
+        public DateTime? To { get; set; }
+    }
+
+    public class FoodSummary
+    {
+        public DateTime Date { get; set; }
+        public decimal CaloriesTotal { get; set; }
+        public decimal ProteinTotal { get; set; }
+        public decimal CarbsTotal { get; set; }
+        public decimal ProteingPercentage => ProteinTotal / (ProteinTotal + CarbsTotal) * 100;
+        public decimal CarbsPercentage => CarbsTotal / (ProteinTotal + CarbsTotal) * 100;
+
+        public bool ProteinGoalAchived => ProteinTotal >= 90;// && ProteinTotal <= 100;
+    }
+}
