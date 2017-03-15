452ded85b0d1062b1018eeae1f59157c7cc6ad65
Fernando Mendoza <fcmendoza@gmail.com>
2017-03-15 08:34:19 -0700

    Added calls to Fitbit Api to get real data.

--- FitbitApi/Controllers/FitbitController.cs
+++ FitbitApi/Controllers/FitbitController.cs
@@ -1,11 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
 using System.Web.Http;
+using FitbitApi.Models;
+using RestSharp;
 
 namespace FitbitApi.Controllers
 {
     public class FitbitController : ApiController
     {
+        public FitbitController()
+        {
+            string baseUrl = "https://api.fitbit.com";
+            _client = new RestClient(baseUrl);
+            _authorizationCode = ConfigurationManager.AppSettings["AuthorizationCode"];
+            _base64EncodedCredentials = ConfigurationManager.AppSettings["Base64EncodedCredentials"];
+    }
+
         [HttpGet]
         public IHttpActionResult Summaries([FromUri] SummaryRequest request)
         {
@@ -32,29 +45,111 @@ namespace FitbitApi.Controllers
         [HttpGet, Route("fitbit/summaries")]
         public IHttpActionResult GetSummaries([FromUri] SummaryRequest request)
         {
-            var summaries = new List<FoodSummary> {
-                new FoodSummary { Date = DateTime.Now.Date.AddDays(-5), CaloriesTotal = 1500, ProteinTotal = 80, CarbsTotal = 180 },
-                new FoodSummary { Date = DateTime.Now.Date.AddDays(-4), CaloriesTotal = 1700, ProteinTotal = 100, CarbsTotal = 200 },
+            InitializeTokens();
+
+            var from = DateTime.Now.AddDays(-14);
+            var to = DateTime.Now;
+
+            Console.WriteLine($"Retrieving daily totals from {from:MMM dd} to {to:MMM dd} ...");
+
+            var summaries = GetFoodSummaries(from, to);
+
+            var averages = new FoodSummary
+            {
+                Date = DateTime.MinValue,
+                CaloriesTotal = summaries.Average(x => x.CaloriesTotal),
+                ProteinTotal = summaries.Average(x => x.ProteinTotal),
+                CarbsTotal = summaries.Average(x => x.CarbsTotal),
             };
+
+            summaries.Add(averages);
+
             return Ok(summaries);
         }
+
+        public List<FoodSummary> GetFoodSummaries(DateTime from, DateTime to)
+        {
+            var summaries = new List<FoodSummary>();
+
+            foreach (var day in EachDay(from, to))
+            {
+                summaries.Add(GetFoodSummary(day));
+            }
+
+            return summaries;
+        }
+
+        private FoodSummary GetFoodSummary(DateTime date)
+        {
+            Console.WriteLine($"Retrieving totals for {date:MMM dd} ...");
+
+            string url = $"1/user/-/foods/log/date/{date:yyyy-MM-dd}.json";
+            var request = new RestRequest(url, Method.GET);
+            request.AddHeader("Authorization", $"Bearer {_accessToken}");
+
+            var response = _client.Execute<FoodResponse>(request);
+
+            if (response.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                if (response.Content.Contains("expired_token"))
+                {
+                    RefreshAccessToken();
+                    GetFoodSummary(date); // retry;
+                }
+            }
+
+            var summary = response.Data.summary;
+
+            return new FoodSummary { Date = date, CaloriesTotal = summary.calories, ProteinTotal = summary.protein, CarbsTotal = summary.carbs };
+        }
+
+        private void InitializeTokens()
+        {
+            // Retrieve last access and refresh tokens from storage.
+            var settings = new SettingsRetriever().Retrieve();
+            _accessToken = settings.AccessToken;
+            _refreshToken = settings.RefreshToken;
+        }
+
+        private void RefreshAccessToken()
+        {
+            string oauthUrl = "oauth2/token";
+
+            var request = new RestRequest(oauthUrl, Method.POST);
+
+            request.AddHeader("Authorization", $"Basic {_base64EncodedCredentials}");
+            request.AddParameter("grant_type", "refresh_token");
+            request.AddParameter("refresh_token", _refreshToken);
+
+            var response = _client.Execute<AccessTokenResponse>(request);
+            var content = response.Content;
+
+            var accessTokenResponse = response.Data;
+
+            _accessToken = accessTokenResponse.access_token;
+            _refreshToken = accessTokenResponse.refresh_token;
+
+            new SettingsRetriever().UpdateSettings(_accessToken, _refreshToken);
+        }
+
+        private IEnumerable<DateTime> EachDay(DateTime from, DateTime thru)
+        {
+            for (var day = from.Date; day.Date <= thru.Date; day = day.AddDays(1))
+                yield return day;
+        }
+
+        private readonly RestClient _client;
+        private string _authorizationCode;
+        private string _accessToken;
+        private string _refreshToken;
+        private string _base64EncodedCredentials;
+        private string _redirectUrl =
+            "https://fitbitfunctionsapp.azurewebsites.net/api/HttpTriggerCSharp1?code=LuvqkWc8OKsA3igutQP3GCha1oRN2Y5ASqgG1PaXwtXYHhyQvLZ6Cg==";
     }
 
     public class SummaryRequest
     {
         public DateTime? From { get; set; }
         public DateTime? To { get; set; }
-    }
-
-    public class FoodSummary
-    {
-        public DateTime Date { get; set; }
-        public decimal CaloriesTotal { get; set; }
-        public decimal ProteinTotal { get; set; }
-        public decimal CarbsTotal { get; set; }
-        public decimal ProteingPercentage => ProteinTotal / (ProteinTotal + CarbsTotal) * 100;
-        public decimal CarbsPercentage => CarbsTotal / (ProteinTotal + CarbsTotal) * 100;
-
-        public bool ProteinGoalAchived => ProteinTotal >= 90;// && ProteinTotal <= 100;
     }
 }
